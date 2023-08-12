a84b8264acb8feb23757bf38a15784de5f23bf0d
Umunnakwe Emmanuel.A <princenayo@gmail.com>
2023-08-12 14:37:04 -0700

    alphABET putchar function

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * Return: 0 (success)
+ */
+int main()
+{
+	char x;
+	char y;
+
+	x = 'a';
+	y = 'A';
+	while
+		(x <= 'z')
+		{
+			putchar(x);
+			x++;
+		}
+	while
+		(y <= 'z')
+		{
+			putchar (y);
+			y++;
+		}
+	putchar('\n');
+	return (0);
+}
