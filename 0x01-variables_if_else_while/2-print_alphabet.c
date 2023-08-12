c0bac28d72f801361a4e8ce6707886bbd1a4f5d8
Umunnakwe Emmanuel.A <princenayo@gmail.com>
2023-08-12 14:19:10 -0700

    Putchar function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+	x = 'a';
+	while
+		(x <= 'z')
+		{
+			putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
