0a8196f548cc660ebb0498951c6832bf65077b53
Umunnakwe Emmanuel.A <princenayo@gmail.com>
2023-08-14 07:23:17 -0700

    A program that prints the alphabet in lowercase without letter q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - starting point 
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char x;
+	x = 'a';
+	while
+		(x <= 'z'){
+			if((x != 'q' && x != 'e') && x <= 'z')
+				putchar(x);
+			x++;
+		}
+	putchar('\n');
+	return (0);
+}
