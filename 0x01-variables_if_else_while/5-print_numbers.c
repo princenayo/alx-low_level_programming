7b276a2d39dacd8ffc25d01ff0c1605140096141
Umunnakwe Emmanuel.A <princenayo@gmail.com>
2023-08-14 08:52:52 -0700

    A program that prints all single digit numbers of base 10 starting from 0,

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int a;
+	
+	for (a = 0; a <= 9; a++)
+		
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
