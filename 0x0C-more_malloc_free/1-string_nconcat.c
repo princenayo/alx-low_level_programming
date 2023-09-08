b2a61737123c0f9bc69c8934acb3afca3d06f2f4
Umunnakwe Emmanuel.A <princenayo@gmail.com>
2023-09-07 22:09:52 -0700

    Write a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,41 @@
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 
 /**
- * string_nconcat - concatenates n bytes of a string to another string
- * @s1: string to append to
- * @s2: string to concatenate from
+ * string_nconcat - to concatenates n bytes of a string to another string
+ * @s1: append to this string
+ * @s2: concatenate from this string
  * @n: number of bytes from s2 to concatenate to s1
  *
  * Return: pointer to the resulting string
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int len1 = 0, len2 = 0;
 
-	while (s1 && s1[len1])
-		len1++;
-	while (s2 && s2[len2])
-		len2++;
+	if (s1 != NULL)
+		len1 = strlen(s1);
 
-	if (n < len2)
-		s = malloc(sizeof(char) * (len1 + n + 1));
-	else
-		s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s2 != NULL)
+		len2 = strlen(s2);
+
+	if (n >= len2)
+		n = len2;
+
+	s = malloc(sizeof(char) * (len1 + n + 1));
 
 	if (!s)
 		return (NULL);
 
-while (i < len1)
-{
-s[i] = s1[i];
-i++;
-}
+	if (len1 > 0)
+		memcpy(s, s1, len1);
 
-while (n < len2 && i < (len1 + n))
-	s[i++] = s2[j++];
+	if (n > 0)
+		memcpy(s + len1, s2, n);
 
-	while (n >= len2 && i < (len1 + len2))
-	s[i++] = s2[j++];
-
-	s[i] = '\0';
+	s[len1 + n] = '\0';
 
 	return (s);
 }
