9664e234018ab8e2deacd71ff285f24edfca6266
Umunnakwe Emmanuel.A <princenayo@gmail.com>
2023-10-02 14:22:30 -0700

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,30 +9,28 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int nayo
-	int numb;
-	int rwr;
+	int dif, numb = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	nayo = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (nayo == -1)
+	dif = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (dif == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
+	if (text_content != NULL)
+	{
+		while (text_content[numb])
+			numb++;
 
-	for (numb = 0; text_content[numb]; numb++)
-		;
+		if (write(dif, text_content, numb) != numb)
+		{
+			close(dif);
+			return (-1);
+		}
+	}
 
-	rwr = write(nayo, text_content, numb);
-
-	if (rwr == -1)
-		return (-1);
-
-	close(nayo);
+	close(dif);
 
 	return (1);
 }
